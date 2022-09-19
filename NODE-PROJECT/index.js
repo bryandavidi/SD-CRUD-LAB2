88fca901d1760045a79462053422118bbde6ae71
Julian Ardila <julian.ardila@uptc.edu.co>
2022-09-19 15:38:14 -0500

    Instalacion dependencias

--- NODE-PROJECT/index.js
+++ NODE-PROJECT/index.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+const express = require('express');
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(port, () => {
+    console.log('Running ' + port)
+})
