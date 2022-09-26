4938e3dc739d8f677f91da6ba68b9bf3cc18882b
Julian Ardila <julian.ardila@uptc.edu.co>
2022-09-26 13:57:07 -0500

    Post, Put metodos Estudiantes

--- NODE-PROJECT/index.js
+++ NODE-PROJECT/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routerApi = require('./routes');
+const bodyParser = require('body-parser');
 const port = 3000;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: true} ))
 
 routerApi(app);
 
