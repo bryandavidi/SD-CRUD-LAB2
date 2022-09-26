6454c03dbe4e714b87b67e1d0767c24204a54905
bryan ibanez <bryan.ibanez@uptc.edu.co>
2022-09-26 15:03:07 -0500

    2.0

--- NODE-PROJECT/index.js
+++ NODE-PROJECT/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const routes = require('./routes');
+const routes_course = require('./routes/routes_course');
+const routes_inscription = require('./routes/routes_inscription');
+const routes_student = require('./routes/routes_student');
 const port = 3000;
 
 const app = express();
@@ -8,7 +10,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to CRUD');
 });
 
-app.use('/api/v1',routes)
+app.use('/api/v1',routes_student)
+app.use('/api/v1',routes_course)
+app.use('/api/v1',routes_inscription)
+
+
 
 
 app.listen(port, () => {
