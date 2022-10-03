fc07883e96f6a75aff6e4ca8da007a7a34feca11
unknown <bryan.ibanez@uptc.edu.co>
2022-10-03 01:58:47 -0500

    conexion a bd en azure

--- NODE-PROJECT/database/database.js
+++ NODE-PROJECT/database/database.js
@@ -2,11 +2,16 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize('sira','Bryan','Sistemasdistribuidos2022',{
 host: 'crud-sd.postgres.database.azure.com',
-dialect: 'postgres',
 port: 5432,
 logging:false,
 native:false,
-sslmode: require
+dialect: 'postgres',
+dialectOptions: {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
+ },
 });
 
 
--- NODE-PROJECT/index.js
+++ NODE-PROJECT/index.js
@@ -4,26 +4,10 @@ const routes_course = require('./routes/routes_course');
 const router_inscription = require('./routes/routes_inscription');
 const routes_student = require('./routes/routes_student');
 
-
 const port = 3000;  
 const app = express();
 app.use(express.json())
 
-// app.get('/', (req, res) => {
-//     res.send('Welcome to CRUD');
-// });
-
-// app.use(sequelize.authenticate);
-
-// app.use('/estudiante',router_student);
-
-// app.use('/inscripciones',router_inscription);
-
-// app.use('/materias',router_course);
-
-// app.listen(port, () => {
-//     console.log('Server Running on port ' + port)
-// })
 app.use('/', routes_student);
 app.use('/curso',routes_course);
 
@@ -31,7 +15,7 @@ async function main(){
     app.listen(port)
     console.log('Servidor corriendo en el puerto : ', port)
     try {
-        await sequelize.sync({force:true})
+        await sequelize.authenticate();
         console.log('Conexion a la base de datos Azure exitosa')
     } catch (error) {
         console.error(error, 'Conexion a la base de datos fallida')
