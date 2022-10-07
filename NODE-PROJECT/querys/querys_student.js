7f0205a9766aeb0892406fe59e6dbbe7bb49bc4f
unknown <bryan.ibanez@uptc.edu.co>
2022-10-06 20:33:56 -0500

    creacion de metodos GET,POST Y DELETE para la tabla estudiantes v.2.0

--- NODE-PROJECT/querys/querys_student.js
+++ NODE-PROJECT/querys/querys_student.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const model_student = require ('../models/model_student');
 
 const getStudents = async(req,res)=>{
@@ -9,4 +10,71 @@ const getStudents = async(req,res)=>{
     }
 };
 
-module.exports = getStudents;
+const getStudentsId = async(req,res)=>{
+    try {
+        const students = await model_student.findAll( 
+            {
+                where :{
+                    id_estudiante : 999
+                } 
+            }
+        );
+        res.json(students);
+    } catch (error) {
+        res.status(500)
+    }
+};
+
+const getStudentsIdCC = async(req,res)=>{
+    try {
+        const students = await model_student.findAll( 
+            {
+                attributes : ['nombres','apellidos','codigo_estudiante','numero_documento' ]
+            }
+        );
+        res.json(students);
+    } catch (error) {
+        res.status(500)
+    }
+};
+
+const createStudent = async(req,res)=>{
+    try {
+        const students = await model_student.create({
+        id_estudiante : "995",
+        codigo_estudiante : "201910225",
+        tipo_documento :"Cedula",
+        numero_documento :"10109101",
+        nombres : "Bryan David ",
+        apellidos :"Ibanez",
+        estado :"Matriculado"})
+
+        res.send('Creando estudiante')
+    } catch (error) {
+        res.status(500);
+    }
+};
+
+
+const deleteStudent = async(req,res)=>{
+    try {
+        const students = await model_student.destroy({
+            where:{
+                id_estudiante : 999
+            }
+        })
+        console.log('Estudiante eliminado')
+    } catch (error) {
+        res.status(500)
+    }
+};
+
+
+
+module.exports.getStudents = getStudents;
+module.exports.getStudentsId = getStudentsId;
+module.exports.getStudentsIdCC = getStudentsIdCC;
+module.exports.createStudent = createStudent;
+module.exports.deleteStudent = deleteStudent;
+
+
--- NODE-PROJECT/routes/routes_student.js
+++ NODE-PROJECT/routes/routes_student.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const routes_student = express.Router();
-const getStudents = require("../querys/querys_student")
+const { getStudents, getStudentsId, getStudentsIdCC, createStudent,deleteStudent } = require("../querys/querys_student");
 
 routes_student.get('/estudiantes/todos',getStudents);
-routes_student.get('/estudiantes/:id');
+routes_student.get('/estudiantes/id',getStudentsId);
+routes_student.get('/estudiantes/idCC',getStudentsIdCC);
+routes_student.post('/estudiantes/crear',createStudent);
+routes_student.delete('/estudiantes/eliminar',deleteStudent);
 
 module.exports = routes_student
