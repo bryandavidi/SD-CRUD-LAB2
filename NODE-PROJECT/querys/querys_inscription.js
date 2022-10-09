e893b17515afa89880665010e358fba6cf669693
unknown <bryan.ibanez@uptc.edu.co>
2022-10-09 17:36:14 -0500

    GET, POST, DELETE tabla inscripciones V2.0

--- NODE-PROJECT/querys/querys_inscription.js
+++ NODE-PROJECT/querys/querys_inscription.js
@@ -9,4 +9,54 @@ const getInscription = async(req,res)=>{
     }
 };
 
-module.exports = getInscription;
+
+const getInscriptionId = async(req,res)=>{
+    try {
+        const inscription = await model_inscription.findAll( 
+            {
+                where :{
+                    id_estudiante : 265,
+                    id_materia : 242 
+                } 
+            }
+        );
+        res.json(inscription);
+    } catch (error) {
+        res.status(500)
+    }
+};
+
+
+const createInscription = async(req,res)=>{
+    try {
+        const inscription = await model_inscription.create({
+        id_estudiante : "265",
+        id_materia : "242",
+        fecha_inscripcion :"2021-09-16T05:02:06.000Z"
+        })
+
+        res.send('Creando Inscripcion')
+    } catch (error) {
+        res.status(500);
+    }
+};
+
+
+const deleteInscription = async(req,res)=>{
+    try {
+        const inscripition = await model_inscription.destroy({
+            where:{
+                id_estudiante : 265,
+                id_materia : 242
+            }
+        })
+        console.log('Inscripcion eliminada')
+    } catch (error) {
+        res.status(500)
+    }
+};
+
+module.exports.getInscription = getInscription;
+module.exports.getInscriptionId = getInscriptionId;
+module.exports.createInscription = createInscription;
+module.exports.deleteInscription = deleteInscription;
